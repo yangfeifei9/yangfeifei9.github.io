a0944ee0f63b685fbb62f20278d0b2ba0d7f10be
yangfeifei9 <110366414+yangfeifei9@users.noreply.github.com>
2022-08-07 17:07:48 +0800

    Add files via upload

---
+++ static/pointclouds/helimap/360/MLS_drive1/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs ",
+    "points": 24907501,
+    "boundingBox": {
+        "lx": 2652130.1920000005,
+        "ly": 1248816.4610000002,
+        "lz": 391.723,
+        "ux": 2652801.2980000006,
+        "uy": 1249487.567,
+        "uz": 1062.8289999999124
+    },
+    "tightBoundingBox": {
+        "lx": 2652130.192,
+        "ly": 1248816.4610000002,
+        "lz": 391.723,
+        "ux": 2652475.927,
+        "uy": 1249487.567,
+        "uz": 426.224
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 5.811948299407959,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
